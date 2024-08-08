b97c555c140e07b767eccc163935198e14ee194b
luymoon <aguiarluana780@gmail.com>
2024-08-08 11:36:18 -0300

    Início do Projeto

---
+++ SnakeGame/App.js
@@ -0,0 +1,16 @@
+import "react-native-gesture-handler";
+import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Game from "./src/components/Game";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <StatusBar style="light" />
+        <Game />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
+  );
+}
